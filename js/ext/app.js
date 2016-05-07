c828f7bef1a73ad2a79b4aaad8d5a3cadc0dbe1c
gokhandemirhan <demirhangokhan@gmail.com>
2016-05-07 20:51:47 +0300

    nutritions info added, loader added. Basics of challenge done.

--- js/ext/app.js
+++ js/ext/app.js
@@ -2,18 +2,16 @@ var SearchForm = React.createClass({
   getInitialState: function () {
     return {
       data:[] ,
-      myFoods:this.props.myFoods
+      myFoods:this.props.myFoods,
+      timer:0,
+      isLoading:false
     }
   },
   handleAdd:function(item){
     var foods = this.props.myFoods;
     foods.push(item);
     localStorage.setItem('SelectedFoods', JSON.stringify(foods));
-
-    var data = this.state.data;
-    data.splice(data.indexOf(item), 1);
-
-    this.setState({ myFoods: foods,data:data });
+    this.setState({ myFoods: foods });
     //console.log(item);
   },
   handleDelete:function(item){
@@ -22,21 +20,26 @@ var SearchForm = React.createClass({
     localStorage.setItem('SelectedFoods', JSON.stringify(foods));
     this.setState({ myFoods: foods });
   },
+  handledeleteAll:function(){
+    //alert here
+    localStorage.setItem('SelectedFoods', JSON.stringify([]));
+    this.setState({ myFoods: []});
+  },
   render:function(){
     var that = this;
+    
     return (<div className="searchForm"><div className="cont">
         <div className="results"><h2>Search Food: </h2>
         <input type="text" placeholder="Search for a food"
-              onChange={that.onChangeHandler} />
+              onChange={that.onChangeHandler} />{ this.state.isLoading ? <div className="loader"></div> : null }
         <ul className="suggestions">{this.state.data.map(function(m){
               return <SearchResultItem key={m.ID} item={m} add={that.handleAdd}/>
           })}
           </ul>
           </div>
-        <div className="myFoods"><h2>Selected Foods:{that.state.myFoods.length}</h2>
-          <ol>
+        <div className="myFoods"><h2>Selected Foods:{that.state.myFoods.length}</h2><button onClick={this.handledeleteAll}>delete all</button>
           {this.state.myFoods.map(function(b){return <SavedFoodsItem key={b.ID} item={b} deleteHandler={that.handleDelete}/>})}
-          </ol></div>
+          </div>
           </div>
       </div>);
   },
@@ -44,12 +47,12 @@ var SearchForm = React.createClass({
      var word = e.target.value,
          that = this;
 
-          var timer;
-          clearTimeout(timer);
     if(word !== ""){
-      
-      timer = setTimeout(function() {
+      clearTimeout(this.state.timer);
+      this.setState({ isLoading: true});
+      this.state.timer = setTimeout(function() {
         query(word).then(function(data){
+          that.setState({ isLoading: false});
           //console.log(data);
           data = JSON.parse(data);
           var items = data.map(function(n){return {ID:n._id,Name:n.name,  Portions:n.portions}});
@@ -99,12 +102,59 @@ var SearchResultItem = React.createClass({
   
 });
 
+
 var SavedFoodsItem = React.createClass({
   handleDelete:function(){
     this.props.deleteHandler(this.props.item)
   },
   render:function(){
-    return <li onClick={this.handleDelete}>{this.props.item.Name} <br/>Servings: <ul>{this.props.item.Portions.map(function(p){return <li>{p['name']}</li>})}</ul></li>
+    return <div><button onClick={this.handleDelete}>delete food</button><Section title={this.props.item.Name}> <br/>Portions: {this.props.item.Portions.map(function(p, i){
+      var nutrients = p['nutrients']; 
+      return <Section title={p['name']} key={i}>{
+        Object.keys(nutrients).map(function (key, t) {
+          return <Section title={key} key={t}>{
+            Object.keys(nutrients[key]).map(function (key2, m) {
+              return <Section title={key2} key={m}>{nutrients[key][key2] != null ? nutrients[key][key2]["value"] + nutrients[key][key2]["unit"] : "0"}</Section>
+            })
+          }</Section>
+        })
+      }
+    </Section>})}</Section></div>
+  }
+});
+
+
+var Section = React.createClass({
+  handleClick: function(){
+    if(this.state.open) {
+      this.setState({
+        open: false,
+        class: "section"
+      });
+    }else{
+      this.setState({
+        open: true,
+        class: "section open"
+      });
+    }
+  },
+  getInitialState: function(){
+     return {
+       open: false,
+       class: "section"
+     }
+  },
+  render: function() {
+    return (
+      <div className={this.state.class}>
+        <div className="sectionhead" onClick={this.handleClick}>{this.props.title}</div>
+        <div className="articlewrap">
+          <div className="article">
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
   }
 });
 
